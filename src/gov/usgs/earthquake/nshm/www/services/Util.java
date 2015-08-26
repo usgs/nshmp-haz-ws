3e98126931b5a038f84748d924cad066432641b0
Peter Powers <pmpowers@usgs.gov>
2015-08-26 08:13:11 -0600

    updates to support multi-period request-response

--- src/gov/usgs/earthquake/nshm/www/services/Util.java
+++ src/gov/usgs/earthquake/nshm/www/services/Util.java
@@ -3,10 +3,16 @@ package gov.usgs.earthquake.nshm.www.services;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
 
+import java.util.EnumSet;
 import java.util.Map;
+import java.util.Set;
+
+import org.opensha2.util.Parsing;
+import org.opensha2.util.Parsing.Delimiter;
 
 import com.google.common.base.Enums;
 import com.google.common.base.Optional;
+import com.google.common.collect.FluentIterable;
 
 class Util {
 
@@ -17,24 +23,31 @@ class Util {
 		LATITUDE,
 		LONGITUDE,
 		IMT;
-		
+
 		private String label;
-		
+
 		private Key() {
 			label = name().toLowerCase();
 		}
-		
+
 		@Override public String toString() {
 			return label;
 		}
 	}
-	
+
 	static <T extends Enum<T>> T readValue(String value, Class<T> type) {
 		Optional<T> opt = Enums.getIfPresent(type, value);
 		checkState(opt.isPresent(), "Invalid value [%s] for enum: %s", value, type.getName());
 		return opt.get();
 	}
 	
+	static <T extends Enum<T>> Set<T> readValues(String values, Class<T> type) {
+		return EnumSet.copyOf(
+			FluentIterable.from(Parsing.split(values, Delimiter.COMMA))
+			.transform(Enums.stringConverter(type))
+			.toList());
+	}
+
 	static String readValue(Map<String, String[]> paramMap, Key key) {
 		String keyStr = key.toString();
 		String[] values = paramMap.get(keyStr);
@@ -44,13 +57,16 @@ class Util {
 	}
 
 	static <T extends Enum<T>> T readValue(Map<String, String[]> paramMap, Key key, Class<T> type) {
-		String value = readValue(paramMap, key);
-		return readValue(value, type);
+		return readValue(readValue(paramMap, key), type);
 	}
-	
+
+	static <T extends Enum<T>> Set<T> readValues(Map<String, String[]> paramMap, Key key,
+			Class<T> type) {
+		return readValues(readValue(paramMap, key), type);
+	}
+
 	static double readDoubleValue(Map<String, String[]> paramMap, Key key) {
 		return Double.valueOf(readValue(paramMap, key));
 	}
-	
 
 }
