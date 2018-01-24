c378cdcb6804efbd947d86c8c2e5e10ba363731d
bclayton-usgs <bclayton@usgs.gov>
2018-01-24 14:53:32 -0700

    GmmDistance.js: Removed hanging wall effect stuff, will put in new webpage

--- webapp/js/GmmDistance.js
+++ webapp/js/GmmDistance.js
@@ -97,14 +97,6 @@ class GmmDistance extends Gmm{
     //--------------------------------------------------------------------------
 
 
-    //......................... Plot Options ...................................
-    let plotOptions = {
-        plotLowerPanel: true,
-        printLowerPanel: false,
-        syncAxis: false    
-    };
-    //--------------------------------------------------------------------------
-
     //....................... Mean Plot Setup ..................................
     meanTooltipText = ["GMM", "Distance (km)", "MGM (g)"];
     meanPlotOptions = {
@@ -118,71 +110,11 @@ class GmmDistance extends Gmm{
     };
     //--------------------------------------------------------------------------
 
-
-    //..................... Sigma Plot Setup ...................................
-    //faultTooltipText = ["GMM", "Period (s)", "SD"];
-    let faultPlotOptions = {
-        marginTop: 50,
-        marginBottom: 20,
-        linewidth: 4,
-        linewidthSelection: 4,
-        plotHeight: 896*0.65/2,
-        plotWidth: 896*0.65,
-        plotRatio: 2/1,
-        pointRadius: 0,
-        pointRadiusSelection: 0,
-        pointRadiusTooltip: 0,
-        showData: false,
-        showLegend: false,
-        transitionDuration: 0,
-        xAxisScale: "linear",
-        xAxisLocation: "top",
-        yAxisScale: "linear",
-    };
-    //--------------------------------------------------------------------------
-  
   
     _this.plot = new D3LinePlot(contentEl,
-        plotOptions,
+        {},
         meanPlotOptions,
-        faultPlotOptions); 
-  
-  
-  
-    d3.select(_this.plot.lowerPanel.svgEl)
-        .style("margin-right", "35%");
-        
-  
-    let faultFormD3 =  d3.select(_this.plot.lowerPanel.plotBodyEl)
-        .append("div")
-        .attr("class", "form form-horizontal fault-form");
-        
-    faultFormD3.append("label")
-        .attr("class", "control-spacer control-group")
-        .attr("for", "dip-slider")
-        .text("Dip:");
-
-    let dipSliderD3 = faultFormD3.append("div")
-        .attr("class", "form-group form-group-sm")
-        
-    dipSliderD3.append("div") 
-        .attr("class", "col-md-8")
-        .html("<input class='slider' id='dip-slider' type='range' " + 
-            "name='dip-slider' min='0' max='90' step='5' />");
-        
-    dipSliderD3.append("div")
-        .attr("class", "col-md-4")
-        .append("div")
-        .attr("class", "input-group input-group-sm")
-        .html("<input class='form-control input-sm' " +
-            "id='dip-slider-value' type='text' readonly >"+
-            "<span class='input-group-addon'> ° </span>")
-    
-
-    $('[data-toggle="tooltip"]').tooltip()
-    _this.dipSliderEl = document.querySelector("#dip-slider");
-    _this.dipSliderValueEl = document.querySelector("#dip-slider-value");
-  
+        {}); 
     
   }
   //------------------ End Method: plotSetup -----------------------------------
@@ -259,56 +191,6 @@ class GmmDistance extends Gmm{
       //------------------------------------------------------------------------
       
       
-      //........................ Plot Fault .................................... 
-      let dip = GmmDistance.dip_val();
-      let width = GmmDistance.width_val();
-      let zTop = GmmDistance.zTop_val();
-      
-      let xMin = 0;
-      let xMax = width * Math.cos(dip);
-      let x = [xMin, Number(xMax.toFixed(4))];
-     
-      let yMin = -zTop;
-      let yMax = - width * Math.sin(dip) - zTop;
-      let y = [yMin, Number(yMax.toFixed(4))];
-      
-      seriesLabels = [];
-      seriesIds = [];
-      seriesData = [];
-        
-      seriesLabels.push("Fault");
-      seriesIds.push("fault");
-      seriesData.push(d3.zip(x, y));
-      
-      _this.plot.lowerPanel.data = seriesData;
-      _this.plot.lowerPanel.ids = seriesIds;
-      _this.plot.lowerPanel.labels = seriesLabels;
-      _this.plot.lowerPanel.metadata = metadata;
-      _this.plot.lowerPanel.xLabel = "km"; 
-      _this.plot.lowerPanel.yLabel = "km";
-      
-      let xDomain = [-1, width];
-      let yDomain = [-width-zTop, 0];
-      _this.plot.plotData(_this.plot.lowerPanel, xDomain, yDomain);
-      //------------------------------------------------------------------------
-     
-      _this.dipSliderEl.value = $("#dip").val();
-      _this.dipSliderValueEl.value = $("#dip").val();
-     
-      _this.dipSliderEl.oninput = function(){
-        $("#dip").val(this.value);
-        _this.dipSliderValueEl.value = this.value;;
-        dip = GmmDistance.dip_val();
-        xMax = width * Math.cos(dip);
-        x = [xMin, Number(xMax.toFixed(4))];
-       
-        yMax = - width * Math.sin(dip) - zTop;
-        y = [yMin, Number(yMax.toFixed(4))];
-        _this.plot.lowerPanel.data = [d3.zip(x, y)];
-        _this.plot.plotData(_this.plot.lowerPanel, xDomain, yDomain);
-      };
-
-
       $(_this.footer.rawBtnEl).off() 
       $(_this.footer.rawBtnEl).click(function(){
         window.open(url);
