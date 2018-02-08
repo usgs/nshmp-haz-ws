46e5fc88041b7a01cf1bc234a5fce00e729b9671
bclayton-usgs <bclayton@usgs.gov>
2018-02-08 14:00:28 -0700

    Config.js: moved default config to apps/config.json

--- webapp/apps/js/lib/Config.js
+++ webapp/apps/js/lib/Config.js
@@ -4,7 +4,7 @@
 * @fileoverview Static method to read two possible config files:
 *     1. /nshmp-haz-ws/apps/js/lib/config.json
 *     2. /nshmp-haz-ws/config.json
-* The first config file, /nshmp-haz-ws/apps/js/lib/config.json, is
+* The first config file, /nshmp-haz-ws/apps/config.json, is
 *     the default config file and remains in the repository.
 * The second config file, /nshmp-haz-ws/config.js, is ignored by github and
 *     is for developmental purposes. If this file exists it will be read in 
@@ -22,7 +22,7 @@ class Config {
   *     new callback(config) will be called. 
   */
   static getConfig(callback){
-    let p1 = $.ajax({url: '/nshmp-haz-ws/apps/js/lib/config.json'});
+    let p1 = $.ajax({url: '/nshmp-haz-ws/apps/config.json'});
     let p2 = $.ajax({url: '/nshmp-haz-ws/config.json'});
     
     $.when(p1, p2).done((c1, c2) => {
