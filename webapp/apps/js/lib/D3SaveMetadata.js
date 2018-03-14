6637e750279c1b4acfd7fbb71cb2b441f8a37007
bclayton-usgs <bclayton@usgs.gov>
2018-03-14 11:46:19 -0600

    D3SaveMetadata: new class to save webpage metadata to a pdf/print

--- webapp/apps/js/lib/D3SaveMetadata.js
+++ webapp/apps/js/lib/D3SaveMetadata.js
@@ -1,23 +1,39 @@
 'use strict';
 
 /**
-* @class SaveMetadata
+* @class D3SaveMetadata
+*
+* @fileoverview Create an iframe and make a table of the metadata and 
+*     print.
 */
 export default class D3SaveMetadata { 
-  
+ 
+  /**
+  * @param {D3SaveMetadataBuilder} builder - The builder.
+  */ 
   constructor(builder) {
     this.filename = builder.filename;
     this.fileFormat = builder.fileFormat;
     this.metadata = builder.metadata;
+  
+    d3.selectAll('.save-metadata-iframe')
+        .remove();
+         
+    let headD3 = d3.select(document.head);
+    
+    let iframeD3 = d3.select('body')
+        .append('iframe')
+        .attr('class', 'hidden save-metadata-iframe');
+        
+    let iframeEl = iframeD3.node();
+   
+    d3.select(iframeEl.contentWindow.document.head)
+        .html(headD3.html());
 
-    d3.select('body')
-        .selectAll('*')
-        .classed('hidden-print', true);
-
-    let metadataD3 = d3.select('body')
+    let metadataD3 = d3.select(iframeEl.contentWindow.document.body)
         .append('div')
-        .attr('class', 'save-metadata-table visible-print-block');
-
+        .attr('class', 'save-metadata-table');
+    
     let tableD3 = metadataD3.append('table')
         .attr('class', 'table table-condensed table-bordered');
 
@@ -35,7 +51,7 @@ export default class D3SaveMetadata {
       if (isArray) {
         tableRowD3.append('ul')
             .style('margin', '0')
-            .style('padding', '0')
+            .style('padding', '5px 0')
             .selectAll('li')
             .data(values)
             .enter()
@@ -51,17 +67,14 @@ export default class D3SaveMetadata {
       }
     }
     
-    let cssD3 = d3.select('head')
-        .append('link')
-        .attr('rel', 'stylesheet')
-        .attr('href', '/nshmp-haz-ws/apps/css/MetadataPrint.css');
-    window.print();
-    cssD3.remove();
-    
-    d3.select('body')
-        .selectAll('*')
-        .classed('hidden-print', false);
-    metadataD3.remove();
+    $(iframeEl).ready(() => {
+      iframeEl.contentWindow.print();
+      
+      iframeEl.contentWindow.onafterprint = () => {
+        d3.select(iframeEl).remove();
+      };
+      
+    });
   }
 
   /**
