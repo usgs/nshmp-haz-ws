d528988a1b118ff203477e565ededb092186e110
bclayton-usgs <bclayton@usgs.gov>
2018-08-23 13:54:49 -0600

    Changed contructor to take x and y values as different arguments

--- webapp/apps/js/d3/data/D3LineSeriesData.js
+++ webapp/apps/js/d3/data/D3LineSeriesData.js
@@ -1,5 +1,7 @@
 
 import D3LineOptions from '../options/D3LineOptions.js';
+import D3XYPair from './D3XYPair.js'; 
+import Preconditions from '../../error/Preconditions.js';
 
 /**
  * @fileoverview Container class to hold XY values and assoiciated 
@@ -11,12 +13,33 @@ import D3LineOptions from '../options/D3LineOptions.js';
 export default class D3LineSeriesData {
 
   /**
-   * @param {Array<Array<Number>>} xyValues The xy values
+   * @param {Array<Number>} xValues The X values
+   * @param {Array<Number>} yValues The Y values
    * @param {D3LineOptions} options The line options
    */
-  constructor(xyValues, options) {
-    this.data = xyValues;
+  constructor(xValues, yValues, options) {
+    Preconditions.checkArgumentArrayOf(xValues, 'number');
+    Preconditions.checkArgumentArrayOf(yValues, 'number');
+    Preconditions.checkArgument(
+        xValues.length == yValues.length, 
+        'Arrays must have same length');
+    Preconditions.checkArgumentInstanceOf(options, D3LineOptions);
+
+    /**
+     * The D3LineOptions associated with XY values
+     * @type {D3LineOptions}
+     */
     this.lineOptions = options;
+
+    /**
+     * The array of XY pair
+     * @type {Array<D3XYPair>}
+     */
+    this.data = [];
+
+    for (let xy of d3.zip(xValues, yValues)) {
+      this.data.push(new D3XYPair(xy[0], xy[1]));
+    }
   }
 
 }
