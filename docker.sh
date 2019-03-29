87479c6477ebc26af671ed6ef089861cd5c45adf
bclayton-usgs <bclayton@usgs.gov>
2019-03-29 16:39:35 -0600

    bash script for docker to download repos

---
+++ docker.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+####
+# Download a USGS repository from Github.
+# Arguments:
+#   (string) repo - The project to download
+#   (string) version - The version to download
+# Returns:
+#   None
+####
+download_repo() {
+  local repo=${1}
+  local version=${2}
+  local url="https://github.com/usgs/${repo}/archive/${version}.tar.gz"
+
+  printf "\n Downloading [${url}] \n"
+  curl -L ${url} | tar -xz
+  mv ${repo}-${version#v*} ${repo}
+}
+
+# Download nshmp-haz
+download_repo "nshmp-haz" ${NSHMP_HAZ_VERSION}
+
+# Download nshm-ak-2007
+download_repo "nshm-ak-2007" ${NSHM_AK_2007_VERSION}
+
+# Download nshm-cous-2008
+download_repo "nshm-cous-2008" ${NSHM_COUS_2008_VERSION}
+
+# Download nshm-cous-2014
+download_repo "nshm-cous-2014" ${NSHM_COUS_2014_VERSION}
+
+# Download nshm-cous-2018
+download_repo "nshm-cous-2018" ${NSHM_COUS_2018_VERSION}
