7203baac5ff2eaa3cfb19eb0c8fc3c735f9edbfb
bclayton-usgs <bclayton@usgs.gov>
2019-08-29 12:36:20 -0600

    waits for zip results

--- src/gov/usgs/earthquake/nshmp/aws/HazardResultsSlicerLambda.java
+++ src/gov/usgs/earthquake/nshmp/aws/HazardResultsSlicerLambda.java
@@ -13,7 +13,12 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
-import com.amazonaws.ClientConfiguration;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
+import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.Reservation;
 import com.amazonaws.services.lambda.AWSLambda;
 import com.amazonaws.services.lambda.AWSLambdaClientBuilder;
 import com.amazonaws.services.lambda.model.InvokeRequest;
@@ -24,7 +29,6 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.google.common.base.Throwables;
-import com.google.gson.JsonObject;
 
 import gov.usgs.earthquake.nshmp.aws.Util.LambdaHelper;
 import gov.usgs.earthquake.nshmp.internal.Parsing;
@@ -43,20 +47,15 @@ import gov.usgs.earthquake.nshmp.www.meta.Status;
 public class HazardResultsSlicerLambda implements RequestStreamHandler {
 
   private static final AmazonS3 S3 = AmazonS3ClientBuilder.defaultClient();
-  private static AWSLambda LAMBDA_CLIENT;
+  private static final AmazonEC2 EC2 = AmazonEC2ClientBuilder.defaultClient();
+  private static final AWSLambda LAMBDA_CLIENT = AWSLambdaClientBuilder.defaultClient();
 
   private static final String LAMBDA_CALL = "nshmp-haz-result-slice";
   private static final String ZIP_LAMBDA_CALL = "nshmp-haz-zip-results";
-  private static final int SDK_TIMEOUT = 10 * 60 * 1000;
+  private static final String INSTANCE_STATUS = "terminated";
 
-  static {
-    ClientConfiguration config = new ClientConfiguration()
-        .withSocketTimeout(SDK_TIMEOUT);
-
-    LAMBDA_CLIENT = AWSLambdaClientBuilder.standard()
-        .withClientConfiguration(config)
-        .build();
-  }
+  private static final int MAX_INSTANCE_CHECK = 100;
+  private static final int INSTANCE_CHECK_TIMEOUT = 10 * 1000;
 
   @Override
   public void handleRequest(
@@ -83,7 +82,7 @@ public class HazardResultsSlicerLambda implements RequestStreamHandler {
 
   private static Response processRequest(
       LambdaHelper lambdaHelper,
-      RequestData request) throws IOException {
+      RequestData request) throws IOException, InterruptedException {
     ObjectListing objectListing = S3.listObjects(request.bucket, request.key);
     List<CompletableFuture<Void>> futures = new ArrayList<>();
 
@@ -101,6 +100,7 @@ public class HazardResultsSlicerLambda implements RequestStreamHandler {
         });
 
     futures.forEach(CompletableFuture::join);
+    lambdaHelper.logger.log("Zipping results");
     zipResults(request);
     return new Response(request);
   }
@@ -159,23 +159,78 @@ public class HazardResultsSlicerLambda implements RequestStreamHandler {
     }
   }
 
-  private static void zipResults(RequestData request) {
+  private static void zipResults(RequestData request) throws InterruptedException {
     InvokeRequest invokeRequest = new InvokeRequest()
         .withFunctionName(ZIP_LAMBDA_CALL)
         .withPayload(GSON.toJson(request));
 
     InvokeResult result = LAMBDA_CLIENT.invoke(invokeRequest);
     checkLambdaResponse(result);
+
+    ZipResultsResponse response = GSON.fromJson(
+        new String(result.getPayload().array()),
+        ZipResultsResponse.class);
+
+    waitForInstance(response);
+  }
+
+  private static void waitForInstance(ZipResultsResponse response) throws InterruptedException {
+    for (int ii = 0; ii < MAX_INSTANCE_CHECK; ii++) {
+      DescribeInstancesRequest request = new DescribeInstancesRequest()
+          .withInstanceIds(response.result.instanceId);
+
+      DescribeInstancesResult instances = EC2.describeInstances(request);
+      if (isTerminated(instances)) {
+        return;
+      }
+
+      Thread.sleep(INSTANCE_CHECK_TIMEOUT);
+    }
+  }
+
+  private static boolean isTerminated(DescribeInstancesResult instances) {
+    for (Reservation reservation : instances.getReservations()) {
+      for (Instance instance : reservation.getInstances()) {
+        if (INSTANCE_STATUS.equals(instance.getState().getName())) {
+          return true;
+        }
+      }
+    }
+
+    return false;
   }
 
   private static void checkLambdaResponse(InvokeResult result) {
-    Object object = GSON.fromJson(
-        new String(result.getPayload().array()),
-        Object.class);
-    JsonObject json = GSON.toJsonTree(object).getAsJsonObject();
-    String status = json.get("status").getAsString();
-    if (Status.ERROR.toString().equals(status)) {
-      throw new RuntimeException(json.get("message").getAsString());
+    try {
+      LambdaResponse response = GSON.fromJson(
+          new String(result.getPayload().array()),
+          LambdaResponse.class);
+
+      if (Status.ERROR.toString().equals(response.status)) {
+        throw new RuntimeException(response.message);
+      }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static class LambdaResponse {
+    String status;
+    String message;
+  }
+
+  private static class ZipResultsResponse extends LambdaResponse {
+    ZipResult result;
+    ZipRequest request;
+
+    private static class ZipRequest {
+      String bucket;
+      String key;
+    }
+
+    private static class ZipResult {
+      String path;
+      String instanceId;
     }
   }
 
